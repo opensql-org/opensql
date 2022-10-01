18d23f9c9694b4cd039aa376b136d8442024a89f
Ali Azmoodeh <treeroot.ir@gmail.com>
2022-10-01 08:46:30 +0330

    change custom type

--- package/type/db/Query.ts
+++ package/type/db/Query.ts
@@ -1,37 +1,46 @@
-export type Where = {
+export type CRUD = {
 
     /**
      * @type string | string[]
-     *
      * It is used to manage the display of columns.
-     *
-     * The @string type is used when you want to manipulate a single column or select all columns using the asterisk word.
+     * The @string type is used when you want to manipulate a single column or3
+     * select all columns using the asterisk word.
      * The @string-array type is used when you want to select and manipulate a multi columns.
      */
     get?: string | string[]
 
     /**
      * @type string | string[]
-     * The name of the table or tables you want to work on
-     *
+     * The name of the table or tables you want to work on.
      * The @string type is used when you want to select single table.
      * The @string-array type is used when you want to select multi tables.
      */
     from?: string | string[]
 
     /**
-     * @type object
+     * @type Where
      * Used to filter record.
-     *
-     * In where object we have list of conditions with columns
+     * In where object we have list of conditions with columns.
      */
-    where?: object
+    where?: Where
+
+    /**
+     * @type Where
+     * Opposite of Where object
+     */
+    whereNot?: Where
 
     /**
      * @type object
-     * Used to insert or update data
+     * Used to insert or update data.
      */
     data?: any[]
+
+    /**
+     * @type object[]
+     * Used to combine the result-set of two or more SELECT statements.
+     */
+    union?: object[]
 
 }
 
@@ -39,7 +48,7 @@ export type Option = {
 
     /**
      * @type string | string[]
-     * Groups rows that have the same values into summary rows
+     * Groups rows that have the same values into summary rows.
      */
     _group?: string | string[]
 
@@ -57,7 +66,60 @@ export type Option = {
 
     /**
      *  @type number
-     *  Used with the ORDER BY clause with a value greater than or equal to zero
+     *  Used with the ORDER BY clause with a value greater than or equal to zero.
      */
     _offset?: number
+
+    /**
+     * @type any
+     * Used to manage specific column.
+     */
+    [key: string]: any
+
+}
+
+type Where = {
+
+    /**
+     * @type any
+     * Used to filter records.
+     */
+    [key: string]: any
+
+    /**
+     * @type string[]
+     * Used to filter records with single or multi (and, or) conditions
+     */
+    op?: string[]
+
+}
+
+export type CreateTable = {
+
+    /**
+     * @type string
+     * Table name.
+     */
+    table: string
+
+    /**
+     * @type object
+     * Used to generate field for table.
+     */
+    field: object
+
+    /**
+     * @type object[]
+     * List of field reference.
+     */
+    ref?: object[]
+
+    /**
+     * @type string[]
+     * Used to search fast in field.
+     */
+    index?: string[]
+
+    primaryKey?: string
+
 }
--- src/common/db/Database.ts
+++ src/common/db/Database.ts
@@ -1,32 +1,32 @@
-import {Where, Option} from '../../../package/type/db/Query';
+import {CRUD, Option} from '../../../package/type/db/Query';
 
 export default abstract class Database {
 
-    abstract find(where?: Where, option?: Option): Promise<any>;
+    abstract find(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract findOne(where?: Where, option?: Option): Promise<any>;
+    abstract findOne(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract findMany(where?: Where, option?: Option): Promise<any>;
-
-
-    abstract update(where?: Where, option?: Option): Promise<any>;
-
-    abstract updateOne(where?: Where, option?: Option): Promise<any>;
-
-    abstract updateMany(where?: Where, option?: Option): Promise<any>;
+    abstract findMany(crud?: CRUD, option?: Option): Promise<any>;
 
 
-    abstract remove(where?: Where, option?: Option): Promise<any>;
+    abstract update(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract removeOne(where?: Where, option?: Option): Promise<any>;
+    abstract updateOne(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract removeMany(where?: Where, option?: Option): Promise<any>;
+    abstract updateMany(crud?: CRUD, option?: Option): Promise<any>;
 
 
-    abstract add(where?: Where): Promise<any>;
+    abstract remove(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract addOne(where?: Where): Promise<any>;
+    abstract removeOne(crud?: CRUD, option?: Option): Promise<any>;
 
-    abstract addMany(where?: Where): Promise<any>;
+    abstract removeMany(crud?: CRUD, option?: Option): Promise<any>;
+
+
+    abstract add(crud?: CRUD): Promise<any>;
+
+    abstract addOne(crud?: CRUD): Promise<any>;
+
+    abstract addMany(crud?: CRUD): Promise<any>;
 
 }
